9fba90dd9d07eed0687e5fe1fc966d548ef4ea18
Luke Wood <34521694+LukeMitchellWood@users.noreply.github.com>
2021-11-30 14:18:43 +1100

    Update recursiveFreeze.js
    added 'function' type check.

--- recursiveFreeze.js
+++ recursiveFreeze.js
@@ -1,7 +1,7 @@
 module.exports = function deepFreeze(obj) {
     const frozen = {}
     Object.keys(obj).forEach(k => {
-        if (typeof obj[k] === 'object') {
+        if (typeof obj[k] === 'object' || typeof obj[k] === 'function') {
             frozen[k] = deepFreeze(Object.freeze(obj[k]));
         }
         return frozen[k] = Object.freeze(obj[k]);
